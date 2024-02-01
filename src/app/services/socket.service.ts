2ff899217ff907cd5028ed67ecf8bfc76b756dd6
yKunaly80 <ykunaly80@gmail.com>
2024-02-01 20:33:08 +0530

    socket and peer connection

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -14,4 +14,39 @@ export class SocketService {
     // console.log('mes==>',message);
     this.socket.emit('mediaSetup', message);
   }
+
+  joinRoom() {
+    this.socket.emit('join-room', 'webrtc1')
+  }
+
+  sendOffer(offer: RTCSessionDescriptionInit) {
+    this.socket.emit('offer', offer)
+  }
+
+  sendAnswer(ans:any) {
+    this.socket.emit('answer', ans)
+  }
+
+  receiveAnswer() {
+    console.log('call==> ans');
+
+    this.socket.on('answer', (answer: any) => {
+      console.log('ans receive==>', answer);
+
+      return answer
+    })
+  }
+
+  sendICECandidate(candidate: RTCIceCandidateInit) {
+    this.socket.emit('ice-candidate', candidate);
+  }
+
+  reciveICECandidate() {
+    this.socket.on('ice-candidate', (candidate: any) => {
+      // peerConnection.addIceCandidate(candidate);
+      return candidate
+    });
+  }
+
+
 }
